8804ae968a67b56bd4a5bf182a69d10614e57a81
Yohsuke Nakano <yohsuke.nak@gmail.com>
2013-09-15 08:20:44 +0900

    grep置換

--- routes/js.js
+++ routes/js.js
@@ -1,3 +1,8 @@
+var config = require('../config.json');
+
 exports.js = function(req, res){
-	res.render('js/js-' + req.params.id, {});
+	res.render('js/js-' + req.params.id, {
+	  	title: config.title,
+	  	instructor: config.instructor
+	});
 };

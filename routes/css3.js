557549e86226e534845755066b43ac4fb6699ce4
Yohsuke Nakano <yohsuke.nak@gmail.com>
2013-09-15 10:01:35 +0900

    リンク修正

--- routes/css3.js
+++ routes/css3.js
@@ -1,3 +1,8 @@
+var config = require('../config.json');
+
 exports.css3 = function(req, res){
-	res.render('css3/css3-' + req.params.id, {});
+	res.render('css3/css3-' + req.params.id, {
+	  	title: config.title,
+	  	instructor: config.instructor
+	});
 };
--- routes/html5.js
+++ routes/html5.js
@@ -1,3 +1,8 @@
+var config = require('../config.json');
+
 exports.html5 = function(req, res){
-	res.render('html5/html5-' + req.params.id, {});
+	res.render('html5/html5-' + req.params.id, {
+	  	title: config.title,
+	  	instructor: config.instructor
+	});
 };

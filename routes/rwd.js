5ad3c9af83b1fd54c2e141696826f77f0f061fd9
Yohsuke Nakano <yohsuke.nak@gmail.com>
2013-09-27 15:12:05 +0900

    diary added

--- routes/rwd.js
+++ routes/rwd.js
@@ -3,6 +3,7 @@ var config = require('../config.json');
 exports.rwd = function(req, res){
 	res.render('rwd/rwd-' + req.params.id + '.jade', {
 	  	title: config.title,
-	  	instructor: config.instructor
+	  	instructor: config.instructor,
+	  	pretty: true	
 	});
 };

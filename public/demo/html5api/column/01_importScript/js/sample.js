7ad7d80d16054c487cf8f10679f5bfc8d0509903
Yohsuke Nakano <yohsuke.nak@gmail.com>
2013-10-18 21:15:21 +0900

    不要ファイル削除

--- public/demo/html5api/column/01_importScript/js/sample.js
+++
@@ -1,5 +0,0 @@
-// Webワーカーを呼び出す
-var myWorker = new Worker("js/sub.js");
-myWorker.addEventListener("message", function(event){
-	document.getElementById("result").innerHTML = event.data;
-}, true);
--- public/demo/html5api/column/02_workers/js/sample.js
+++
@@ -1,9 +0,0 @@
-// ワーカー内でのオブジェクトチェック
-window.addEventListener("load", function(){
-	var time = (new Date()).getTime();
-	myWorker = new Worker("js/test.js?t="+time);
-	myWorker.addEventListener("message", function(event){
-		document.getElementById("stat").innerHTML = event.data;
-	}, true);
-	myWorker.postMessage(null);	// ワーカーの処理を開始
-}, true);

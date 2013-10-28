710a1d7b460ca6bb439c76c56fa7f1de034688b3
Yohsuke Nakano <yohsuke.nak@gmail.com>
2013-10-29 00:13:18 +0900

    削除

--- routes/css3.js
+++
@@ -1,9 +0,0 @@
-var config = require('../config.json');
-
-exports.css3 = function(req, res){
-	res.render('css3/css3-' + req.params.id + '.jade', {
-	  	title: config.title,
-	  	instructor: config.instructor,
-	  	pretty: true
-	});
-};

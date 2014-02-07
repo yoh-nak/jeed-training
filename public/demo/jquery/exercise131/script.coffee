8daa867561f2d752cd06137d2709426ce4a2d30e
Yohsuke Nakano <yohsuke.nak@gmail.com>
2014-02-07 12:28:42 +0900

    jquery

--- public/demo/jquery/exercise131/script.coffee
+++ public/demo/jquery/exercise131/script.coffee
@@ -2,6 +2,20 @@ $ ->
 	$("input").click ->
 		$("table").css "display", "none"
 		$("ul").css "display", "block"
+
+		$("ul").html "\
+			<li>" + $("tr td:nth-child(1):parent:eq(0)").html() + "</li>\
+			<li>" + $("tr td:nth-child(1):parent:eq(1)").html() + "</li>\
+			<li>" + $("tr td:nth-child(2):parent:eq(0)").html() + "</li>\
+			<li>" + $("tr td:nth-child(2):parent:eq(1)").html() + "</li>\
+			<li>" + $("tr td:nth-child(3):parent:eq(0)").html() + "</li>\
+			<li>" + $("tr td:nth-child(3):parent:eq(1)").html() + "</li>\
+			<li>" + $("tr td:nth-child(4):parent:eq(0)").html() + "</li>\
+			<li>" + $("tr td:nth-child(4):parent:eq(1)").html() + "</li>\
+			<li>" + $("tr td:nth-child(5):parent:eq(0)").html() + "</li>\
+			<li>" + $("tr td:nth-child(5):parent:eq(1)").html() + "</li>"
+
+		###
 		html = "<li>" + $("tr td:nth-child(1):parent:eq(0)").html() + "</li>"
 		html += "<li>" + $("tr td:nth-child(1):parent:eq(1)").html() + "</li>"
 		html += "<li>" + $("tr td:nth-child(2):parent:eq(0)").html() + "</li>"
@@ -12,7 +26,25 @@ $ ->
 		html += "<li>" + $("tr td:nth-child(4):parent:eq(1)").html() + "</li>"
 		html += "<li>" + $("tr td:nth-child(5):parent:eq(0)").html() + "</li>"
 		html += "<li>" + $("tr td:nth-child(5):parent:eq(1)").html() + "</li>"
+
 		$("ul").html html
+		###
+
+		###
+		html = ""
+		i = 1
+
+		while i <= 5
+			j = 0
+
+			while j <= 1
+				html += "<li>" + $("tr td:nth-child(" + i + "):parent:eq(" + j + ")").html() + "</li>"
+				j++
+			i++
+
+		$("ul").html html
+		###
+
 		return
 
 	return
--- public/demo/jquery/exercise131/script.js
+++ public/demo/jquery/exercise131/script.js
@@ -2,20 +2,49 @@
 (function() {
   $(function() {
     $("input").click(function() {
-      var html;
       $("table").css("display", "none");
       $("ul").css("display", "block");
-      html = "<li>" + $("tr td:nth-child(1):parent:eq(0)").html() + "</li>";
-      html += "<li>" + $("tr td:nth-child(1):parent:eq(1)").html() + "</li>";
-      html += "<li>" + $("tr td:nth-child(2):parent:eq(0)").html() + "</li>";
-      html += "<li>" + $("tr td:nth-child(2):parent:eq(1)").html() + "</li>";
-      html += "<li>" + $("tr td:nth-child(3):parent:eq(0)").html() + "</li>";
-      html += "<li>" + $("tr td:nth-child(3):parent:eq(1)").html() + "</li>";
-      html += "<li>" + $("tr td:nth-child(4):parent:eq(0)").html() + "</li>";
-      html += "<li>" + $("tr td:nth-child(4):parent:eq(1)").html() + "</li>";
-      html += "<li>" + $("tr td:nth-child(5):parent:eq(0)").html() + "</li>";
-      html += "<li>" + $("tr td:nth-child(5):parent:eq(1)").html() + "</li>";
-      $("ul").html(html);
+      $("ul").html("\
+        <li>" + $("tr td:nth-child(1):parent:eq(0)").html() + "</li>\
+        <li>" + $("tr td:nth-child(1):parent:eq(1)").html() + "</li>\
+        <li>" + $("tr td:nth-child(2):parent:eq(0)").html() + "</li>\
+        <li>" + $("tr td:nth-child(2):parent:eq(1)").html() + "</li>\
+        <li>" + $("tr td:nth-child(3):parent:eq(0)").html() + "</li>\
+        <li>" + $("tr td:nth-child(3):parent:eq(1)").html() + "</li>\
+        <li>" + $("tr td:nth-child(4):parent:eq(0)").html() + "</li>\
+        <li>" + $("tr td:nth-child(4):parent:eq(1)").html() + "</li>\
+        <li>" + $("tr td:nth-child(5):parent:eq(0)").html() + "</li>\
+        <li>" + $("tr td:nth-child(5):parent:eq(1)").html() + "</li>");
+
+      /*
+      		html = "<li>" + $("tr td:nth-child(1):parent:eq(0)").html() + "</li>"
+      		html += "<li>" + $("tr td:nth-child(1):parent:eq(1)").html() + "</li>"
+      		html += "<li>" + $("tr td:nth-child(2):parent:eq(0)").html() + "</li>"
+      		html += "<li>" + $("tr td:nth-child(2):parent:eq(1)").html() + "</li>"
+      		html += "<li>" + $("tr td:nth-child(3):parent:eq(0)").html() + "</li>"
+      		html += "<li>" + $("tr td:nth-child(3):parent:eq(1)").html() + "</li>"
+      		html += "<li>" + $("tr td:nth-child(4):parent:eq(0)").html() + "</li>"
+      		html += "<li>" + $("tr td:nth-child(4):parent:eq(1)").html() + "</li>"
+      		html += "<li>" + $("tr td:nth-child(5):parent:eq(0)").html() + "</li>"
+      		html += "<li>" + $("tr td:nth-child(5):parent:eq(1)").html() + "</li>"
+      
+      		$("ul").html html
+       */
+
+      /*
+      		html = ""
+      		i = 1
+      
+      		while i <= 5
+      			j = 0
+      
+      			while j <= 1
+      				html += "<li>" + $("tr td:nth-child(" + i + "):parent:eq(" + j + ")").html() + "</li>"
+      				j++
+      			i++
+      
+      		$("ul").html html
+       */
     });
   });
 

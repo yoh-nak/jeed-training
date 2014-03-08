44b517ac2d790908203032ed33a7fe4977135eee
Yohsuke Nakano <yohsuke.nak@gmail.com>
2014-03-08 15:16:02 +0900

    jqm

---
+++ public/demo/jqm/company/js/script21.js
@@ -0,0 +1,13 @@
+$(document).on('pageshow','#access',function(){
+    var myLatlng = new google.maps.LatLng(35.658609,139.745447);
+    var myOptions = {
+        zoom: 15,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), myOptions);
+    var marker = new google.maps.Marker({
+        position: myLatlng, 
+        map: map, 
+    });
+});
